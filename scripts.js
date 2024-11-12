212ef7092dbee24775b8a10307c8748f691233db
ImoEazy <imieadams@gmail.com>
2024-11-12 11:36:10 +0200

    added object mapping

--- scripts.js
+++ scripts.js
@@ -13,6 +13,37 @@ names.forEach((name, index) => {
   console.log(`${name} (${provinces[index]})`);
 }); //logs names with matching provinces in the format "Name (Province)"
 
+//exercise 2 - Uppercase Transformation: 
+const uppercasedProvinces = provinces.map(province => province.toUpperCase())
+console.log(uppercasedProvinces); //logs Provinces in uppercase
+
+//exercise 3 - Name Lengths:
+const nameLength = names.map(name => name.length)
+console.log(nameLength) //logs the length of each name
+
+//exercise 4 - Sort Provinces in Alphabetical order 
+const sortedProvinces = provinces.sort()
+console.log(sortedProvinces) //logs provinces in alphabetical order
+
+//exercise 5 - filter to remove provinces containing "Cape" 
+const nonCapeProvinces = provinces.filter(province => !province.includes("Cape"))
+console.log(nonCapeProvinces.length) //logs the count for provinces without the word "Cape"
+
+//exercise 6 - Finding 'S' determine if name contains letter 'S'
+const nameWithS = names.map((name) => {
+  return name.split('').some((char) => char === 'S');
+});
+console.log(nameWithS) //logs boolean array- displays true for name containing 's' else false
+
+//exercise 7 - creating object mapping Jan-Hendrik has ' ' - fix also make sure it is an object 
+const namesToProvincesMap = names.reduce((acc, name, index) => {
+  acc[name] = provinces[index];
+  return acc;
+}, {});
+console.log(namesToProvincesMap); //logs an object mapping names to their respective provinces
+
+
+
 // A list of products with prices:
 const products = [
   { product: 'banana', price: "2" },
@@ -24,9 +55,9 @@ const products = [
 ]
 
 // 1. ForEach basics
-provinces.forEach(province => console.log(province));
-names.forEach(name => console.log(name));
-names.forEach((name, index) => console.log(`${name} (${provinces[index]})`));
+//provinces.forEach(province => console.log(province));
+//names.forEach(name => console.log(name));
+//names.forEach((name, index) => console.log(`${name} (${provinces[index]})`));
 
 //2. Uppercase transformation
-console.log(provinces.map(provinces => provinces.toUpperCase()));
+//console.log(provinces.map(provinces => provinces.toUpperCase()));
